91ee2117955a86f8eb20f2f4c8b3f09957a95be7
a1c-lsibanda <leslie.sibanda@a1comms.com>
2021-03-24 14:13:03 +0000

    added database migrations to workflow

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,5 +7,10 @@ module.exports = {
     variants: {
         extend: {}
     },
-    plugins: [require("@tailwindcss/forms")]
+    plugins: [
+        require('@tailwindcss/ui'),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/aspect-ratio")
+    ]
 };
