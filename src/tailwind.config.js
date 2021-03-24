2d23676967bfb44eb15e8f324b7b05eaf423001d
a1c-lsibanda <leslie.sibanda@a1comms.com>
2021-03-24 15:37:57 +0000

    downgradded tailwind, postcss and autoprefixer

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
         extend: {}
     },
     plugins: [
-        // require('@tailwindcss/ui'),
         require("@tailwindcss/forms"),
         require("@tailwindcss/typography"),
         require("@tailwindcss/aspect-ratio")
